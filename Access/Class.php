85cdb65db708f0ae933e03aab4d16a104bafe131
PetrP <prochazkapp@gmail.com>
2011-10-19 05:44:28 +0200

    AccessClass: methods a properties se cachuji

--- Access/Class.php
+++ Access/Class.php
@@ -2,6 +2,9 @@
 
 class AccessClass extends AccessBase
 {
+	private $methods = array();
+	private $properties = array();
+
 	public function __construct($object)
 	{
 		parent::__construct($object, new ReflectionClass($object));
@@ -9,24 +12,46 @@ class AccessClass extends AccessBase
 
 	public function __call($name, $args)
 	{
-		$a = new AccessMethod($this->reflection->getName(), $name);
-		$a->asInstance($this->instance);
-		return $a->callArgs($args);
+		if (!isset($this->methods[$name]))
+		{
+			$a = new AccessMethod($this->reflection->getName(), $name);
+			$this->methods[$name] = $a->asInstance($this->instance);
+		}
+		return $this->methods[$name]->callArgs($args);
 	}
 
 	public function & __get($name)
 	{
-		$a = new AccessProperty($this->reflection->getName(), $name);
-		$a->asInstance($this->instance);
-		$tmp = $a->get($name);
+		if (!isset($this->properties[$name]))
+		{
+			$a = new AccessProperty($this->reflection->getName(), $name);
+			$this->properties[$name] = $a->asInstance($this->instance);
+		}
+		$tmp = $this->properties[$name]->get($name);
 		return $tmp;
 	}
 
 	public function __set($name, $value)
 	{
-		$a = new AccessProperty($this->reflection->getName(), $name);
-		$a->asInstance($this->instance);
-		return $a->set($value);
+		if (!isset($this->properties[$name]))
+		{
+			$a = new AccessProperty($this->reflection->getName(), $name);
+			$this->properties[$name] = $a->asInstance($this->instance);
+		}
+		return $this->properties[$name]->set($value);
 	}
 
+	public function asInstance($object)
+	{
+		parent::asInstance($object);
+		foreach ($this->methods as $a)
+		{
+			$a->asInstance($this->instance);
+		}
+		foreach ($this->properties as $a)
+		{
+			$a->asInstance($this->instance);
+		}
+		return $this;
+	}
 }
