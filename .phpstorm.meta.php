a61fee6774ff39eab634e58d3633a4ae48079a17
PetrP <prochazkapp@gmail.com>
2020-02-02 01:10:05 +0100

    Meta: add support for PhpStorm with `.phpstorm.meta.php` or plugin `DynamicReturnTypePlugin`

---
+++ .phpstorm.meta.php
@@ -0,0 +1,10 @@
+<?php
+/** @link https://www.jetbrains.com/help/phpstorm/ide-advanced-metadata.html */
+
+namespace PHPSTORM_META;
+
+override(\Access(0), type(0));
+override(\Access(0), map(['' => '$0']));
+override(\AccessProxy(0), type(0));
+override(\AccessProxy(0), map(['' => '$0']));
+override(\AccessClass::asInstance(0), type(0));
