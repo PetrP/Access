7e611db732f52a7e4f8b6708237dcbdeb7f26377
PetrP <prochazkapp@gmail.com>
2023-02-12 20:19:30 +0100

    Tests: remove all libs from repository (will be replaced with composer)

--- tests/libs/HttpPHPUnit/ResultPrinter/ResultPrinterTestCaseHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-abstract class ResultPrinterTestCaseHelper extends PHPUnit_Framework_TestCase
-{
-	static public function _getDataSetAsString(PHPUnit_Framework_TestCase $test)
-	{
-		return $test->getDataSetAsString();
-	}
-}
